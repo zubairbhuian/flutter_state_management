9ba1bed9d51a05c5f8bd7c1fdd088eb758a0af7f
Zubair <zubairbhuian@gmail.com>
2022-02-24 13:38:29 +0600

    24.2

---
+++ lib/screens/product_overview_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class ProductOverviewScreen extends StatelessWidget {
+  const ProductOverviewScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
