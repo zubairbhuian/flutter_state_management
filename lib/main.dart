3abded0968e97c7173184464c63a6df0eb8433f4
Zubair <zubairbhuian@gmail.com>
2022-02-24 13:01:20 +0600

    24.1

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(appBar: AppBar(title: const Text("Demo App"),),
+      body: const Center(child: Text("This is demo app")),
+      ),
+
+      
+    );
+  }
+}
